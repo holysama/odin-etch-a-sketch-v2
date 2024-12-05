25db15c904c26145222c137683ac167d2e6e54ee
holysama <97112425+holysama@users.noreply.github.com>
2024-12-06 00:29:14 +0100

    feat: create 16x16 grid

---
+++ index.js
@@ -0,0 +1,15 @@
+//creating a 16x16 grid
+
+let flexContainer = document.querySelector(".flex-container");
+
+for (let i = 0; i < 16; i++) {
+    let column = document.createElement("div");
+    column.classList.add("columnDiv");
+    flexContainer.appendChild(column);
+    
+    for (let j = 0; j < 16; j++) {
+        let row = document.createElement("div");
+        row.classList.add("rowDiv");
+        column.appendChild(row);
+    }
+}
