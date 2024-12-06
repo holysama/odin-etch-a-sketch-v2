f0aa31a93836693cb3d057bc6c26eb16dddef667
holysama <97112425+holysama@users.noreply.github.com>
2024-12-06 18:46:03 +0100

    feat: add createGrid to create a grid based on user input

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 let gridContainer = document.querySelector(".grid-container");
 let btnContainer = document.querySelector(".btn-container");
 
-//grid button based on user input
+//make a grid / grid button based on user input
+
+function createGrid(gridNum) {
+    gridContainer.replaceChildren();
+    for (let i = 0; i < gridNum; i++) {
+        let column = document.createElement("div");
+        column.classList.add("columnDiv");
+        gridContainer.appendChild(column);
+        
+        for (let j = 0; j < gridNum; j++) {
+            let row = document.createElement("div");
+            row.classList.add("rowDiv");
+            column.appendChild(row);
+        }
+    }
+    fixedColorChange();
+}
+
+function checkValue(num) {
+    if (num >= 1 && num <= 100 && !isNaN(num)) {
+        createGrid(num);
+    }
+    else {
+        getValuePrompt();
+    }
+}
+
+function getValuePrompt() {
+    let value = Number(prompt("Enter the number of squares per side for the new grid (1-100):"));
+    checkValue(value);
+}
 
 let createGridBtn = document.createElement("button");
 createGridBtn.textContent = "Create a New Grid";
+createGridBtn.addEventListener("click", getValuePrompt);
 btnContainer.appendChild(createGridBtn);
 
 
@@ -20,13 +51,16 @@ for (let i = 0; i < 16; i++) {
         row.classList.add("rowDiv");
         column.appendChild(row);
     }
+    fixedColorChange();
 }
 
-//square color change
+//square fixed color change function
 
-let squares = document.querySelectorAll(".rowDiv");
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "grey";
+function fixedColorChange() {
+    let squares = document.querySelectorAll(".rowDiv"); //have to make this a fucntion that i can recall
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "grey";
+        });
     });
-});
+}
